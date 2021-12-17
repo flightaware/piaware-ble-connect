1eff9e192367e2ff015dc13be02841fa54e2fc98
eric1tran <eric1tran@gmail.com>
2021-12-17 20:53:12 +0000

    Add a restart_systemd_service and some cleanup

--- piaware-ble-connect/services.py
+++ piaware-ble-connect/services.py
@@ -1,11 +1,8 @@
-""" This file contains helper functions to interact with systemd services using Python's D-Bus library
+""" This file contains helper functions to enable/disable BLE services
 
 """
-import dbus
 import logging
 import subprocess
-
-from constants import DBUS_SYSTEMD_IFACE, DBUS_SYSTEMD_OBJECT_PATH, DBUS_SYSTEMD_MANAGER_IFACE, DBUS_PROP_IFACE, DBUS_SYSTEMD_UNIT_IFCE
 
 logger = logging.getLogger('piaware_ble_connect')
 
@@ -15,13 +12,17 @@ def stop_systemd_service(service_name):
     cmd = ["sudo", "systemctl", "stop", service_name]
     subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-
 def start_systemd_service(service_name):
     logger.debug(f'Starting {service_name}')
 
     cmd = ["sudo", "systemctl", "start", service_name]
     subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
+def restart_systemd_service(service_name):
+    logger.debug(f'Restarting {service_name}')
+
+    cmd = ["sudo", "systemctl", "restart", service_name]
+    subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 def shutdown_ble_services():
     logger.info(f'Stopping PiAware Bluetooth LE services for piaware configuration')
@@ -34,7 +35,11 @@ def start_piaware_configurator():
     logger.debug(f'Starting piaware-configurator...')
     start_systemd_service('piaware-configurator.service')
 
+def restart_piaware_configurator():
+    logger.debug(f'Restarting piaware-configurator...')
+    restart_systemd_service('piaware-configurator.service')
 
 def start_piaware_wifi_scan():
     logger.debug(f'Starting piaware-wifi-scan...')
     start_systemd_service('piaware-wifi-scan.service')
+
