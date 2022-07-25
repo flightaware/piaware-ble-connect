72a192ba4fde35ae575710800c5a2ba5f7fc4f5e
eric1tran <eric1tran@gmail.com>
2022-07-25 14:57:06 +0000

    Use an ExecPreStart script to save the current sysfs trigger setting. Restore this setting whenever we disable BLE pairing

--- piaware-ble-connect/led.py
+++ piaware-ble-connect/led.py
@@ -8,38 +8,14 @@ def blink(led_num):
     cmd = f"echo timer | sudo tee /sys/class/leds/{led_num}/trigger 1>/dev/null"
     os.system(cmd)
 
+
+# Read default trigger saved in /run/ directory
 def restore_default(led_num):
-
-    default_triggers = {
-        '9000c1': 'actpwr', # Pi Zero W
-        '902120': 'actpwr', # Pi Zero 2 W
-        '9020e0': 'mmc0',   # Pi 3A+
-        'a02100': 'mmc0',   # Pi Compute Module 3
-        'a02082': 'mmc0',   # Pi 3B
-        'a22082': 'mmc0',   # Pi 3B
-        'a32082': 'mmc0',   # Pi 3B
-        'a020d3': 'mmc0',   # Pi 3B+
-        'a02100': 'mmc0',   # Pi Compute Module 3+
-        'a03111': 'mmc0',   # Pi 4 1gb
-        'b03111': 'mmc0',   # Pi 4 2gb
-        'c03111': 'mmc0'    # Pi 4 3gb
-    }
-
-    revision = None
-
-    # Get cpu revision
-    with open('/proc/cpuinfo', 'r') as f:
-        cpuinfo = f.readlines()
-        for line in cpuinfo:
-            fields = line.split(':')
-            if fields[0].strip() == 'Revision':
-                revision = fields[1].strip()
-                break
-
-    # Set trigger based on Pi's default. Default to mmc0 if we can't lookup revision
-    if revision in default_triggers:
-        trigger = default_triggers[revision]
-    else:
+    try:
+        with open('/run/piaware-ble-connect/trigger', 'r') as f:
+            trigger = f.read()
+    except FileNotFoundError as e:
+        # Default to mmc0 if we can't read the file
         trigger = 'mmc0'
 
     cmd = f"echo {trigger} | sudo tee /sys/class/leds/{led_num}/trigger 1>/dev/null"
